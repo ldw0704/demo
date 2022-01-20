c1b8c958a12e0a0802f55bc375d8f87f52db2ba5
이동욱 <ldw0704@gmail.com>
2022-01-20 13:37:22 +0900

    01.20

--- src/Client.java
+++ src/Client.java
@@ -1,9 +1,43 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Client {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	final static String SERVER_IP = "127.0.0.1";
+	// 상수만들때쓰는 final, 상수변수명은 다 대문자, 단어사이는 언더바.
+	// final 이 들어가면 변경 불가.
+	final static int SERVER_PORT = 1225;
+	final static String MESSAGE_TO_SERVER = "Hi, Server";
 
+	public static void main(String[] args) {
+		Socket socket = null;
+		try {
+			socket = new Socket(SERVER_IP, SERVER_PORT);// try/catch
+			System.out.println("socket 연결");
+
+			InputStream is = socket.getInputStream();
+			OutputStream os = socket.getOutputStream();
+
+			os.write(MESSAGE_TO_SERVER.getBytes());
+			os.flush();
+
+			byte[] data = new byte[16];
+			int n = is.read(data);
+			final String messageFromServer = new String(data, 0, n); // data 만큼만 읽어들인다.
+			System.out.println(messageFromServer);
+
+			is.close();
+			os.close();
+
+			socket.close(); // 반드시 열면 닫아주자.
+
+		} catch (Exception e) { // 예외처리 반드시 해당 변수가 처리
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
---
+++ src/Server.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server extends Thread { // extends Thread 추가
+
+	final static int SERVER_PORT = 1225;
+	final static String MESSAGE_TO_CLINET = "Hi, Client";
+
+	public static void main(String[] args) {
+		ServerSocket serverSocket = null;
+		try { // 타이핑보단 눌러서 띄우자
+			serverSocket = new ServerSocket(SERVER_PORT);
+
+			while (true) {
+				System.out.println("socket 연결 대기");
+				Socket socket = serverSocket.accept();
+				System.out.println("host:" + socket.getInetAddress() + "통신 연결성공");
+
+				InputStream is = socket.getInputStream();
+				OutputStream os = socket.getOutputStream();
+
+				byte[] data = new byte[16];
+				int n = is.read(data);
+				final String messageFromClient = new String(data, 0, n); // data 만큼만 읽어들인다.
+				System.out.println(messageFromClient);
+
+				os.write(MESSAGE_TO_CLINET.getBytes());
+				os.flush();
+				
+				is.close();
+				os.close();  // 읽고 출력하는 부분 인풋스트림부터 클로즈까지
+
+				socket.close();
+			}
+
+		} catch (Exception e) { // 앞의 IO지워줄것
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
